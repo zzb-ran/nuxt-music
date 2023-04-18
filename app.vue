48012f186d9c8a7299492f55f3a47b2b56ceae75
zzb-ran <zzbranran@gmail.com>
2023-04-18 10:19:45 +0800

    update: delete comment code

--- app.vue
+++ app.vue
@@ -33,35 +33,6 @@
       <v-main>
         <v-container class="py-8 px-6" fluid>
           <LazyNuxtPage />
-          <!-- <v-row>
-            <v-col v-for="card in cards" :key="card" cols="12">
-              <v-card>
-                <v-list lines="two">
-                  <v-list-subheader>{{ card }}</v-list-subheader>
-                  <template v-for="n in 6" :key="n">
-                    <v-list-item>
-                      <template v-slot:prepend>
-                        <v-avatar color="grey-darken-1"></v-avatar>
-                      </template>
-
-                      <v-list-item-title>Message {{ n }}</v-list-item-title>
-
-                      <v-list-item-subtitle>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Nihil repellendus distinctio similique
-                      </v-list-item-subtitle>
-                    </v-list-item>
-
-                    <v-divider
-                      v-if="n !== 6"
-                      :key="`divider-${n}`"
-                      inset
-                    ></v-divider>
-                  </template>
-                </v-list>
-              </v-card>
-            </v-col>
-          </v-row> -->
         </v-container>
       </v-main>
     </v-app>
@@ -69,12 +40,11 @@
 </template>
 
 <script setup>
-const cards = ref(["Today", "Yesterday"]);
 const drawer = ref(null);
 const links = [
   ["mdi-inbox-arrow-down", "Inbox"],
   ["mdi-send", "Send"],
   ["mdi-delete", "Trash"],
-  ["mdi-alert-octagon", "Spam"],
+  ["mdi-alert-octagon", "Spam"]
 ];
 </script>
