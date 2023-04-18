b08675f28372f37f3700b46264d6e71dae3c9089
zzb-ran <zzbranran@gmail.com>
2023-04-18 11:17:42 +0800

    update:add 按需引入组件

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { HookResult } from "nuxt/schema";
+import vuetify from "vite-plugin-vuetify";
+
 export default defineNuxtConfig({
   runtimeConfig: {
     // The private keys which are only available server-side
-    apiSecret: '123',
+    apiSecret: "123",
     // Keys within public are also exposed client-side
     public: {
-      apiBase: 'http://localhost:4000/api'
-    },
+      apiBase: "http://localhost:4000/api"
+    }
   },
   build: {
-    transpile: ['vuetify', 'mdi/font']
+    transpile: ["vuetify", "@mdi/font"]
   },
-})
+  modules: [
+    async (options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        if (config.plugins !== undefined) {
+          config.plugins.push(vuetify());
+        }
+      });
+    }
+  ]
+});
