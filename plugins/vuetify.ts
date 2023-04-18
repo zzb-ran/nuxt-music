fbee4ca812768c9b4dff2a9bd50ed2c83b635c88
zzb-ran <zzbranran@gmail.com>
2023-04-18 11:16:54 +0800

    update:delete import all component

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,22 +1,10 @@
 import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
-import { aliases, mdi } from "vuetify/iconsets/mdi";
-import '@mdi/font/css/materialdesignicons.css'
+import "@mdi/font/css/materialdesignicons.css";
 import "vuetify/styles";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
-    ssr: true,
-    components,
-    directives,
-    icons: {
-      defaultSet: "mdi",
-      aliases,
-      sets: {
-        mdi,
-      },
-    },
+    ssr: true
   });
 
   nuxtApp.vueApp.use(vuetify);
